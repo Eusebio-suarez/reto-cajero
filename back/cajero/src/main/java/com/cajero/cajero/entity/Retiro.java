cf1135984e7ba2862b41866d211b68be9c2d0ba5
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-08-28 08:26:45 -0500

    modificar entidad retiro

--- back/cajero/src/main/java/com/cajero/cajero/entity/Retiro.java
+++ back/cajero/src/main/java/com/cajero/cajero/entity/Retiro.java
@@ -1,14 +1,17 @@
 package com.cajero.cajero.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="retiros")
 public class Retiro {
 
     @Id
